3d7b37f817f5a6adb391e54c42dcbb8a83763cd5
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-12-18 11:24:35 +0100

    Add tests by choosing and typing options in searching hotels

--- src/test/java/SearchHotelTest.java
+++ src/test/java/SearchHotelTest.java
@@ -21,6 +21,27 @@ public class SearchHotelTest {
         driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
 
+        // testing select date by typing
+
+        driver.findElement(By.cssSelector("input[name='checkin']")).sendKeys("25/12/2025");
+        driver.findElement(By.cssSelector("input[name='checkout']")).sendKeys("30/12/2025");
+        driver.findElement(By.cssSelector("input[name='travellers']")).sendKeys("2");
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-block.btn-primary.pfb0.loader")).click();
+
+
+        //testing select date by choosing
+
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.cssSelector("input[name='checkin']")).click();
+        driver.findElement(By.xpath("/html/body/div[9]/div[1]/table/tbody/tr[5]/td[1]")).click();
+        driver.findElement(By.xpath("/html/body/div[10]/div[1]/table/tbody/tr[5]/td[3]")).click();
+        driver.findElement(By.cssSelector("input[name='travellers']")).click();
+        driver.findElement(By.id("adultMinusBtn")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-block.btn-primary.pfb0.loader")).click();
+
+
+
 
 
 
