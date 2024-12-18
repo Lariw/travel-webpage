5d6842866ea88606bb1ae2089657cc57c11a6cca
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-12-18 12:13:36 +0100

    Added assertion for names all hotels in Dubai

--- src/test/java/SearchHotelTest.java
+++ src/test/java/SearchHotelTest.java
@@ -2,8 +2,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SearchHotelTest {
@@ -35,16 +37,16 @@ public class SearchHotelTest {
         driver.findElement(By.cssSelector("input[name='checkin']")).click();
         driver.findElements(By.xpath("//td[@class='day ' and text()='25']"))
                         .stream()
-                        .filter(el -> el.isDisplayed())
+                        .filter(WebElement::isDisplayed)
                         .findFirst()
-                        .ifPresent(el -> el.click());
+                        .ifPresent(WebElement::click);
 
         driver.findElement(By.cssSelector("input[name='checkout']")).click();
         driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
                         .stream()
-                        .filter(el -> el.isDisplayed())
+                        .filter(WebElement::isDisplayed)
                         .findFirst()
-                        .ifPresent(el -> el.click());
+                        .ifPresent(WebElement::click);
 
         driver.findElement(By.cssSelector("input[name='travellers']")).click();
         driver.findElement(By.id("adultMinusBtn")).click();
@@ -52,11 +54,18 @@ public class SearchHotelTest {
         driver.findElement(By.id("adultPlusBtn")).click();
         driver.findElement(By.id("adultPlusBtn")).click();
         driver.findElement(By.cssSelector(".btn.btn-lg.btn-block.btn-primary.pfb0.loader")).click();
+        List<String> hotelsResult =  driver.findElements(By.cssSelector("div.row >h4 > a > b"))
+                .stream()
+                .map(el -> el.getAttribute("textContent"))
+                .toList();
 
+        System.out.println("Number of hotels avaliably in Dubai: " + hotelsResult.size());
+        hotelsResult.forEach(System.out::println);
 
-
-
-
+        Assert.assertEquals(hotelsResult.get(0), "Jumeirah Beach Hotel");
+        Assert.assertEquals(hotelsResult.get(1), "Oasis Beach Tower");
+        Assert.assertEquals(hotelsResult.get(2), "Rose Rayhaan Rotana");
+        Assert.assertEquals(hotelsResult.get(3), "Hyatt Regency Perth");
 
     }
 
